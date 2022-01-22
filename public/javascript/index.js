20bb3527ae881f4d3af0827a70f80fdfbfd7f7a5
Molly Montgomery <Molly@mollys-air-2.lan>
2022-01-22 13:29:14 -0600

    working on API call

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,20 @@
-var getBook = function(query) {
-    var apiUrl = (`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-    // make a get request to url
-    fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayBooks(data);
-            });
-        }
-    })
-};
-
-var displayBooks = function(data) {
-    var display = document.querySelector('#book-container') //need to add that to HTML
-
-    var bookName = document.createElement('h2');
-    bookName.textContent = data.volumeInfo.title &&
-    data.volumeInfo.authors &&
-    data.volumeInfo.description &&
-    data.volumeInfo.imageLinks.thumbnail
-}
 var bookSearchBtn = document.querySelector('.bookSearchBtn')
-bookSearchBtn.addEventListener("click",getBook)
-displayBooks();
-module.exports = displayBooks
+var display = document.querySelector('#book-container')
+var bookContainer = document.getElementById('book-container')
+var bookName = document.querySelector("")
+
+var getBook = function(bookName) {
+    event.preventDefault();
+    console.log("THIS IS WORKING");
+    var apiUrl = (`https://www.googleapis.com/books/v1/volumes?q=${bookName}`);
+    fetch(apiUrl).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        document.querySelector(data.volumeInfo);
+    })
+    console.log(bookName);
+}
+
+bookSearchBtn.addEventListener("click",getBook);
+
+
