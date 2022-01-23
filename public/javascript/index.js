20189ec438c0877d712a42528a5e20be812a9c8a
Ronya <klmirg6@gmail.com>
2022-01-23 14:04:01 -0600

    fixing an error

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,25 +1,46 @@
+let bookContainer = document.getElementById("book-container")
 
+function searchFormHandler(event) {
+    console.log('HELLO');
+    event.preventDefault();
+    const searchInput = document.querySelector('input[name="title"]').value.trim();
+    console.log(searchInput);
 
-var getBook = function(query) {
-    var apiUrl = (`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-    // make a get request to url
-    fetch(apiUrl).then(function(response) {
-        // request was successful
+    var apiBook = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput;
+
+    fetch(apiBook).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
-                displayBooks(data);
-            });
+            return response.json()
         }
+    }).then(function(data) {
+        console.log(data);
+        searchedResult(data)
     })
-};
-
-var displayBooks = function(data) {
-    var display = document.querySelector('#book-container') //need to add that to HTML
-
-    var bookName = document.createElement('h2');
-    bookName.textContent = data.volumeInfo.title &&
-    data.volumeInfo.authors &&
-    data.volumeInfo.description &&
-    data.volumeInfo.imageLinks.thumbnail
 }
 
+function searchedResult(data){
+    console.log("This is where the searched resulsts will go")
+    bookContainer.innerText=""
+    let title = document.createElement("h2")
+    title.innerText = data.items[0].volumeInfo.title
+    title.setAttribute("class", "title")
+    bookContainer.appendChild(title)
+
+    let bookList = document.createElement("ul")
+
+    bookContainer.appendChild(bookList)
+
+    let lists = data.items[0].volumeInfo.authors
+    for (let i = 0; i < lists.length; i++) {
+        console.log("THIS IS WHERE THE BOOK WILL GO")
+        let bookList = document.createElement("li")
+        bookList.setAttribute("class" ,"bookList")
+    
+        bookList.appendChild(title)
+        bookList.innerText= lists[i]
+    }
+};
+
+
+
+document.querySelector('.search').addEventListener('submit', searchFormHandler);
