5de28049e397d972efc06da959ebc1897ab17b4c
Molly Montgomery <Molly@mollys-air-2.lan>
2022-01-22 23:12:32 -0600

    added searchedResult function and new divs

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,5 @@
+let bookContainer = document.getElementById("book-container")
+
 function searchFormHandler(event) {
     console.log('HELLO');
     event.preventDefault();
@@ -12,36 +14,33 @@ function searchFormHandler(event) {
         }
     }).then(function(data) {
         console.log(data);
-        // searchedResult(data)
+        searchedResult(data)
     })
 }
 
+function searchedResult(data){
+    console.log("This is where the searched resulsts will go")
+    bookContainer.innerText=""
+    let title = document.createElement("h2")
+    title.innerText = data.items[0].volumeInfo.title
+    title.setAttribute("class", "title")
+    bookContainer.appendChild(title)
+
+    let bookList = document.createElement("ul")
+
+    bookContainer.appendChild(bookList)
+
+    let lists = data.items[0].volumeInfo.authors
+    for (let i = 0; i < lists.length; i++) {
+        console.log("THIS IS WHERE THE BOOK WILL GO")
+        let bookList = document.createElement("li")
+        bookList.setAttribute("class" ,"bookList")
+    
+        bookList.appendChild(title)
+        bookList.innerText= lists[i]
+    }
+};
 
 
 
 document.querySelector('.search').addEventListener('submit', searchFormHandler);
-
-// // function searchBook(){
-// //     let searchInput=$("#search-input").val()
-
-// //     var apiBook = "https://www.googleapis.com/books/v1/volumes?q="+searchInput
-
-// //     fetch (apiBook).then(function(response){
-// //         if (response.ok){
-// //             return response.json()
-// //         }
-// //     }).then(function(data){
-// //         searchedResult(data)
-// //     })
-// // }
-// // function searchedResult(data){
-// //     event.preventDefault();
-// //     bookContainer.innerHTML=""
-// //     let title = document.createElement("h2")
-// //     title.innerText = data.hits[0].volumeId.title
-// //     title.setAttribute("class", "title")
-// //     bookContainer.appendChild(title)
-// // }
-
-// // searchButton.addEventListener("submit", searchBook)
-
