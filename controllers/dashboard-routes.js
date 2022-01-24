3b7cdc9d5c39ea4b8b88affbc1b6749148a4338c
Ronya <klmirg6@gmail.com>
2022-01-24 02:06:18 -0600

    trying to get favorites to work

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -39,6 +39,43 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-router.post('')
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'favorite_book',
+      'comment_text'
+    ],
+    include: [
+      // {
+      //   model: Comment,
+      //   attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      //   include: {
+      //     model: User,
+      //     attributes: ['username']
+      //   }
+      // },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (dbPostData) {
+      const post = dbPostData.get({ plain: true });
+  
+      res.render('edit-post', {
+        post,
+        loggedIn: true
+      });
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
