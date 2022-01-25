bbf2655192266dd1ea78cbd3f81263649f1fa7c2
Ronya Gettis <90957681+klmirg@users.noreply.github.com>
2022-01-25 00:02:21 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('bookshelf_db', "root", "102206Jeremiah!",  {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, ,  {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
