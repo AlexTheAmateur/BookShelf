e15c2b51e1879c3c4eb11b74a3f3f1f50c49714b
Ronya <klmirg6@gmail.com>
2022-01-24 21:44:18 -0600

    fixed the broken code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,8 +42,38 @@ router.get('/', (req, res) => {
 //   res.render('homepage', {
 //     loggedIn: req.session.loggedIn
 //   });
-// });
 
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'favorite_book',
+      'comment_text'
+    ],
+    include: [
+      // {
+      //   model: Comment,
+      //   attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      //   include: {
+      //     model: User,
+      //     attributes: ['username']
+      //   }
+      // },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
 router.get('/post/:id', (req, res) => {
   Post.findOne({
@@ -90,6 +120,21 @@ router.get('/post/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('dashboard', { 
+        post,
+        loggedIn: req.session.loggedIn
+     });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 router.get('/signup', (req, res) => {
   res.render('signup');
